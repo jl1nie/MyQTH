6c785b58b46f17fe5c14e5bf502a9b06f81841ac
w-ockham <minoru.tomobe@gmail.com>
2022-08-13 22:29:55 +0900

    Fix JAFF-0094.

--- addannotation.py
+++ addannotation.py
@@ -8,11 +8,13 @@ con = sqlite3.connect('jaffpota.db')
 cur = con.cursor()
 
 def setotable(jaffid, uid):
-    pattern = ['JAFF-0024','JAFF-0114','JAFF-0115','JAFF-0024,0115','JAFF-0114,0115','JAFF-0024,0114,0115',
-    'JAFF-0104,105','JAFF-0106','JAFF-0101','JAFF-0102','JAFF-0103','JAFF-0108','JAFF-0109','JAFF-0110','JAFF-0111',
-    'JAFF-0112','JAFF-0113']
+    pattern = [
+        'JAFF-0024','JAFF-0114','JAFF-0115','JAFF-0024,0115','JAFF-0114,0115',
+        'JAFF-0024,0114,0115','JAFF-0104,105','JAFF-0106','JAFF-0101','JAFF-0102',
+        'JAFF-0103','JAFF-0108','JAFF-0109','JAFF-0110','JAFF-0111',
+        'JAFF-0112','JAFF-0113', 'JAFF-0094']
     setodict = {
-        14:12, 23:10, 24:9, 55:2, 56:1, 57:1, 58:2, 59:1, 60:1, 61:1, 62:2, 63:1, 64:2,
+        14:12, 28:18, 29:18, 23:10, 24:9, 55:2, 56:1, 57:1, 58:2, 59:1, 60:1, 61:1, 62:2, 63:1, 64:2,
         65:2, 66:1, 67:2, 68:3, 69:1, 70:3, 71:3, 72:3, 73:3, 74:3, 
         75:1, 76:2, 77:3, 78:1, 79:1, 80:1, 81:3, 82:4, 83:2, 84:3,
         85:4, 86:1, 87:3, 88:3, 89:1, 90:2, 91:2, 92:3, 93:3, 95:1,
@@ -41,6 +43,9 @@ for elem in src['objects']['jaffpota']['geometries']:
        (pota, jaff, name, location, locid, type, level, namek, lat, lng) = res
        namek = re.sub(r'\(.*\)','',namek)
        namek = re.sub(r'（.*）','',namek)
+       jaff_replaced = setotable(jaff, uid)
+       if jaff_replaced == 'JAFF-0094':
+           namek = '妙高戸隠連山国立公園'
        elem['properties'] = { 'JAFF': setotable(jaff, uid) , 'POTA': pota ,'UID':uid ,'NAME': namek}
     else:
         print(f'Fatal error: {jaff}')
