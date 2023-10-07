907cb35a54066d2821aa9a137ef75332c8633dc3
w-ockham <minoru.tomobe@gmail.com>
2023-10-07 14:58:41 +0900

    Return cached response for latest same query.

--- revgeocoder.js
+++ revgeocoder.js
@@ -4,21 +4,38 @@ const endpoint = {
     'muni' : 'https://www.sotalive.net/api/reverse-geocoder/LonLatMuniToAddress',
 };
 
+var cache_result = null;
+var prev_pos = null;
+
 async function local_reverse_geocoder(lat, lng, elev) {
-    let pos = '?lat=' + String(lat) + '&lon=' + String(lng)
+    let pos = '?lat=' + String(lat) + '&lon=' + String(lng);
+
+    if (prev_pos && prev_pos == pos && cache_result)
+	return cache_result;
+
+    prev_pos = pos;
+	    
     let rev_uri = endpoint['revgeocode'] + pos
     let elev_uri = endpoint['elevation'] + pos + '&outtype=JSON'
     let res_elev = null;
     if (elev) 
 	res_elev = fetch(elev_uri);
-    res = await fetch(rev_uri);
+
+    let res = await fetch(rev_uri);
     res = await res.json();
+    if (!('results' in res)) {
+	cache_result = {'errors': 'OUTSIDE_JA'}; 
+	return cache_result; 
+    }
+    
     let muni_uri =
 	endpoint['muni'] + pos + '&muni=' + res['results']['muniCd'];
-    res2 = await fetch(muni_uri);
+    let res2 = await fetch(muni_uri);
     let result = await res2.json()
     result['addr1'] = res['results']['lv01Nm']
     result['errors'] = 'OK'
+    cache_result = result;
+    
     if (elev) {
 	return res_elev
 	    .then(res => res.json())
@@ -27,6 +44,7 @@ async function local_reverse_geocoder(lat, lng, elev) {
 		result['hsrc'] = res['hsrc']
 		if (res['elevation'] == '-----')
 		    result['errors'] = 'OUTSIDE_JA';
+		cache_result = result;
 		return result;})
     } else
 	return new Promise((resolve, reject) => { resolve(result);});
